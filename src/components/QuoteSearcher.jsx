d8cf9d9db17f2124518e3038c12260887ce5de8d
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-25 15:55:29 +0100

    counted number of likes and dislikes

--- src/components/QuoteSearcher.jsx
+++ src/components/QuoteSearcher.jsx
@@ -28,6 +28,7 @@ export default class QuoteSearcher extends Component {
         console.error("error!", err);
       });
   };
+
   like = id => {
     const quotesLiked = this.state.quotes.map(quote => {
       if (id === quote.id) {
@@ -51,9 +52,30 @@ export default class QuoteSearcher extends Component {
   };
 
   render() {
+    const likesCount = this.state.quotes.reduce((total, current) => {
+      if (current.liked === true) {
+        total = total + 1;
+        return total;
+      } else {
+        return total;
+      }
+    }, 0);
+
+    const dislikesCount = this.state.quotes.reduce((total, current) => {
+      if (current.disliked === true) {
+        total = total + 1;
+        return total;
+      } else {
+        return total;
+      }
+    }, 0);
+
     return (
       <div>
         <h1>Quotes</h1>
+        <p>
+          Likes: {likesCount} / Dislikes: {dislikesCount}
+        </p>
         {this.state.fetching ? "Loading..." : null}
         {this.state.quotes.map(quote => (
           <Quote data={quote} like={this.like} dislike={this.dislike} />
